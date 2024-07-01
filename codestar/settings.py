bcbc53d4e0c277377df754634d0313124420e27b
John Lynch <johnf.lynch@yahoo.ie>
2024-07-01 16:50:45 +0000

    post blog

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-carlow78-djangoblog-ch2cps43l74.ws.codeinstitute-ide.net",".herokuapp.com"]
 
@@ -87,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
