0075d264697da6b71044fd465dae38a8658f2415
John Lynch <johnf.lynch@yahoo.ie>
2024-07-02 13:11:43 +0000

    debugging the db

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ["-created_on"]
@@ -32,7 +33,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
+    
     class Meta:
         ordering = ["created_on"]
 
